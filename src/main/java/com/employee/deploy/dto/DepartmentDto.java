a34e8551db6b12b08459b53cb16208d082c729ce
nahye <knh8783@gmail.com>
2025-06-18 14:52:38 +0900

    add mapper, exception

--- src/main/java/com/employee/deploy/dto/DepartmentDto.java
+++ src/main/java/com/employee/deploy/dto/DepartmentDto.java
@@ -1,5 +1,10 @@
 package com.employee.deploy.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Getter
 @Setter
 @NoArgsConstructor
