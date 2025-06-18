5b488c874a5b1d8ce38c2fdffd0a77d3eae42e23
nahye <knh8783@gmail.com>
2025-06-18 14:38:35 +0900

    add dto

---
+++ src/main/java/com/employee/deploy/dto/DepartmentDto.java
@@ -0,0 +1,11 @@
+package com.employee.deploy.dto;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentDto {
+    private Long id;
+    private String departmentName;
+    private String departmentDescription;
+}
